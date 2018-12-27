1a2e45509fa40cd3440db351696fde457a2c4f3f
Кирилл Абрамов <work.kirillabramov@gmail.com>
2018-12-27 16:39:20 +0200

    Updates

---
+++ precache-manifest.35c09252ec90abd345656448296479bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8bc597f4901866cc259",
+    "url": "/react-todo/static/css/main.61c902db.chunk.css"
+  },
+  {
+    "revision": "c8bc597f4901866cc259",
+    "url": "/react-todo/static/js/main.c8bc597f.chunk.js"
+  },
+  {
+    "revision": "32b13b7b05c9815e8f9c",
+    "url": "/react-todo/static/js/1.32b13b7b.chunk.js"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "842fa273525249d99aaf71dc4777a726",
+    "url": "/react-todo/index.html"
+  }
+];
